2d1199d7e61b6dc41391c385793e6c56974df98a
Shriram Krishnamurthi <shriram@gmail.com>
2025-11-13 21:00:57 -0500

    adjust mapping of prefs to costs to avoid really big numbers

--- assign.jl
+++ assign.jl
@@ -34,11 +34,11 @@ numDays = length(daySize)
 
 function convert_choice(choice)
     if startswith(choice, "Really, absolutely")  # can't make it!
-        return 10000000                          # would be nice to use Inf, but default solver expects finite values
+        return 10000                          # would be nice to use Inf, but default solver expects finite values
     elseif choice == "Third choice"
-        return 10000
-    elseif choice == "Second choice"
         return 100
+    elseif choice == "Second choice"
+        return 10
     elseif choice == "First choice"
         return 1
     else
